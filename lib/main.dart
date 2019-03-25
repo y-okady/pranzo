d1eb06ce621bc3e91e51868465bab6bcd2ea1650
Yuki Okada <y.okady.221@gmail.com>
2019-03-25 21:44:41 +0900

    delete sample code

--- lib/main.dart
+++ lib/main.dart
@@ -48,8 +48,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  DateTime _lastSignedInAt = null;
+  DateTime _lastSignedInAt;
   FirebaseUser _user;
   GoogleMapController _mapController;
 
@@ -92,17 +91,6 @@ class _MyHomePageState extends State<MyHomePage> {
   void _handleMapCreate(GoogleMapController controller) {
     setState(() {
       _mapController = controller;
-    });
-  }
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
     });
   }
 
@@ -159,13 +147,6 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
             RaisedButton(
               color: Colors.primaries[0],
               onPressed: () {
@@ -193,11 +174,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
